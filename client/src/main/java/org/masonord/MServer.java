12679a4504c5015a56ec8beaccfee5f96fe7d0f8
MasoNord <trotf233@gmail.com>
2024-02-19 13:27:06 +0300

    refactoring

---
+++ client/src/main/java/org/masonord/MServer.java
@@ -0,0 +1,8 @@
+package org.masonord;
+
+public class MServer {
+    public static void main(String[] args) {
+        Server server = new Server();
+        server.run();
+    }
+}
---
+++ server/src/main/java/org/masonord/MClient.java
@@ -0,0 +1,8 @@
+package org.masonord;
+
+public class MClient {
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.run();
+    }
+}
